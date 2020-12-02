f0ec02f830ec0a20d7a0366361e606a16c6b98fb
Joseph <josephoc17@gmail.com>
2020-12-02 17:33:41 +0000

    AOC Day 1: C solution, first draft

---
+++ 1/day1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// TODO: Define array size at compile time
+int main(void) {
+
+    int lineNum;
+    int lineArray[200];
+    FILE *myfile = fopen("input.txt", "r");
+    if (myfile == NULL) {
+        printf("Cannot open file.\n");
+        return 1;
+    }
+    else {
+        //Check for number of line
+            char ch;
+            int count = 0;
+        do
+        {
+        ch = fgetc(myfile);
+        if (ch == '\n') count++;
+        } while (ch != EOF);
+        rewind(myfile);
+
+        int i;
+        for (i = 0; i < count; i++) {
+            fscanf(myfile, "%d\n", &lineArray[i]);
+        }
+    }
+
+    int n = sizeof(lineArray)/sizeof(lineArray[0]);
+    int i, j;
+    for (i=0; i < n; i++){
+        for (j=0;j < n-i-1;j++){
+            if((lineArray[i]+lineArray[j]) == 2000){
+                printf("Numbers are %d and %d, product is %d\n",lineArray[i],lineArray[j],lineArray[i]*lineArray[j]);
+            }
+        }
+    }
+
+}
